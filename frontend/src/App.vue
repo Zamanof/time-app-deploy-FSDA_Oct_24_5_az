3c4f555fabd7d3d3db6eaeeb821da97fc203a6c1
Nadir Zamanov <123265575+Zamanof@users.noreply.github.com>
2026-05-10 17:36:59 +0400

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Time from './components/Time.vue'
+</script>
+
+<template>
+  <Time :showSavedTimes="true" />
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  margin: 100px auto;
+}
+
+.green {
+  color: rgb(107, 44, 255);
+}
+</style>
